b0ac6eff3548818ef16e377d041574dea6a13b71
LORDPRITHWISH <prithwishchakraborty05@gmail.com>
2025-02-14 23:44:45 +0530

    sss

--- src/app/demo/[no]/page.tsx
+++ src/app/demo/[no]/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 import Temp1 from "@/components/template/Temp1";
-import { useParams } from "next/navigation";
 import Temp2 from "@/components/template/Temp2";
-import Temp4 from "@/components/template/Temp4";
 import Temp3 from "@/components/template/Temp3";
+import Temp4 from "@/components/template/Temp4";
+import Temp5 from "@/components/template/Temp5";
+import { useParams } from "next/navigation";
 
 const title = "Love Bliss";
 
@@ -46,8 +47,6 @@ export default function Home() {
   const { no } = useParams();
   console.log(no);
 
-  // const celebrationMediaUrl =
-    // "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExOXdweTYyYWk3ZGp2bDNjcGo5c2QwdnNmN3U0dWFucHo2dmZ1Z2dyMyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/DorxfW5xBGSG8bVxRa/giphy.gif";
   
     const celebrationMediaUrl =
       "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExNzMxbWtuYTFldDdtd3I3MmxzMXc4ZjJneHA2dWVsODJndXlwYTBqdyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/UkfUod3TFW66J2BWKK/giphy.gif";
@@ -83,25 +82,37 @@ export default function Home() {
     case "3":
       return (
         <Temp3
-          // title={title}
-          // messages={messages}
-          // moods={moods}
-          // prev={false}
-          // noButtonMessages={noButtonMessages}
-          // celebrationMediaUrl={celebrationMediaUrl}
-          // celebrationMessage={celebrationMessage}
+          title={title}
+          messages={messages}
+          moods={moods}
+          prev={false}
+          noButtonMessages={noButtonMessages}
+          celebrationMediaUrl={celebrationMediaUrl}
+          celebrationMessage={celebrationMessage}
         />
       );
     case "4":
       return (
         <Temp4
-          // title={title}
-          // messages={messages}
-          // moods={moods}
-          // prev={false}
-          // noButtonMessages={noButtonMessages}
-          // celebrationMediaUrl={celebrationMediaUrl}
-          // celebrationMessage={celebrationMessage}
+          title={title}
+          messages={messages}
+          moods={moods}
+          prev={false}
+          noButtonMessages={noButtonMessages}
+          celebrationMediaUrl={celebrationMediaUrl}
+          celebrationMessage={celebrationMessage}
+        />
+      );
+    case "5":
+      return (
+        <Temp5
+          title={title}
+          messages={messages}
+          moods={moods}
+          prev={false}
+          noButtonMessages={noButtonMessages}
+          celebrationMediaUrl={celebrationMediaUrl}
+          celebrationMessage={celebrationMessage}
         />
       );
     default:
--- src/components/template/Temp4.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Temp4() {
-  return (
-    <div>Temp4</div>
-  )
-}
